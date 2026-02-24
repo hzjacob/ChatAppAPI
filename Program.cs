c53a6a9faa483a39d4a5cc1e404f88447fe45238
hzjacob <ktogrul17@gmail.com>
2026-02-24 16:26:24 +0400

    web app connection

--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@ var supabaseKey = builder.Configuration["Supabase:Key"];
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+options.AddPolicy("BlazorPolicy", policy =>
+{
+    policy.WithOrigins("https://localhost:7232","http://localhost:5298")
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials();
+} ));
 #pragma warning disable CS8604 // Possible null reference argument.
 builder.Services.AddSingleton(_ => new Supabase.Client(supabaseURL, supabaseKey, new Supabase.SupabaseOptions
 {
@@ -15,6 +23,10 @@ builder.Services.AddSingleton(_ => new Supabase.Client(supabaseURL, supabaseKey,
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseCors("BlazorPolicy");
+
+app.UseAuthorization();
 app.MapControllers();
 
 // Configure the HTTP request pipeline.
